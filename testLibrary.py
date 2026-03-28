454c699108785524c400d4c2d8acb2c804a27cad
CSM <Christianskaftemortensen@gmail.com>
2026-03-28 02:02:35 +0100

    fixed inconsistent terms and structures

--- testLibrary.py
+++ testLibrary.py
@@ -4,16 +4,16 @@ from Library import Book, Member, Library, LibraryItem
 #Fixtures
 
 @pytest.fixture
-def sample_library():
+def sample_Book():
     return Book("1984", "George Orwell", 5, "1234567890")
 
 @pytest.fixture
-def single_sample_Library():
+def single_sample_Book():
     return Book("To Kill a Mockingbird", "Harper Lee", 1, "0987654321")
 
 @pytest.fixture
 def sample_member():
-    return Member("John Doe", "johndoe@example.com", "12345")
+    return Member("John Doe", "12345", "johndoe@example.com")
 
 @pytest.fixture
 def lib_with_data():
@@ -30,9 +30,10 @@ class TestBook:
     def test_creation(self, sample_book):
         assert sample_book.title == "1984"
         assert sample_book.author == "George Orwell"
-        assert sample_book.book_id == "1234567890"
+        assert sample_book.book_isbn == "1234567890"
+        
         assert sample_book.copies == 5
-        assert sample_book.available_copies == 5
+        assert sample_book.available == 5
 
     def test_negative_copies_raises(self):
         with pytest.raises(ValueError):
@@ -49,12 +50,10 @@ class TestBook:
 
     def test_repr(self, sample_book):
        assert "1984" in repr(sample_book)
-       assert "George Orwell" in repr(sample_book)
-       assert "1234567890" in repr(sample_book)
-       assert "5" in repr(sample_book)
+       
 
 # Member Tests
-    class TestMember:
+class TestMember:
         def test_creation(self, sample_member):
             assert sample_member.name == "John Doe"
             assert sample_member.email == "johndoe@example.com"
@@ -104,7 +103,7 @@ class TestBook:
             assert "returned" in result
             
 ## Library Book Management
-
+class TestLibraryBookManagement:
     def test_add_book(self, lib_with_data):
         assert "1234567890" in lib_with_data.books
         assert "0987654321" in lib_with_data.books
@@ -180,7 +179,7 @@ class TestMemberManagement:
                 lib_with_data.remove_member("67890")
                 
 ## Circulation Management tests
-    class TestCirculationManagement:
+class TestCirculationManagement:
         def test_issue_books_decrements_available(self, lib_with_data):
             lib_with_data.issue_book("12345", "1234567890")
             assert lib_with_data.books["1234567890"].available == 4
